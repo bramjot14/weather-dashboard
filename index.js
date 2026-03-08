29fefe804ca547bebbb43179a540a1d31429f51d
bramjot14 <bramjot9874@gmail.com>
2026-03-07 20:54:19 -0500

    build weather dashboard using Express, EJS and OpenWeather API

---
+++ index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import axios from "axios";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+const API_KEY = "b5ffaef7a4cfdaccf0dcc115529679d7";
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", async (req, res) => {
+   const lat=43.6548;
+   const lon=-79.3884
+    try {
+      const result = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`);
+      res.render("index.ejs", {
+        city: result.data.name,
+        temp: result.data.main.temp,
+        feels_like: result.data.main.feels_like,
+        wind: result.data.wind.speed,
+        direction: result.data.wind.deg,
+        humidity: result.data.main.humidity,
+        clouds: result.data.clouds.all,
+        pressure: result.data.main.pressure
+      }
+        );
+    } catch (error) {
+        console.log(error.message);
+        res.status(500);
+      }
+  });
+  
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
